62d6dd5b83550847d16019f1554fca4a19471a49
Oussama Ben Guirat <benguiratoussama1998@gmail.com>
2024-11-03 13:17:55 +0100

    function that counts the words of any book

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+# This is the main function that reads the file and prints the contents
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
 
-#main()
+# This function counts the words in the file
+def count_words():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        return len(words)
+
+print(count_words())
